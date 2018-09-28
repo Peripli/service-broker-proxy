eaf9eefe0765855056d52946790afbef96a1337a
Kiril Kabakchiev <kiril.kabakchiev@sap.com>
2018-09-28 16:01:39 +0300

    Tests (#19)

--- pkg/sm/sm_suite_test.go
+++ pkg/sm/sm_suite_test.go
@@ -17,21 +17,13 @@
 package sm
 
 import (
-	"encoding/json"
+	"testing"
 
-	osbc "github.com/pmorie/go-open-service-broker-client/v2"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-// Brokers type used for responses from the Service Manager client
-type Brokers struct {
-	Brokers []Broker `json:"brokers"`
-}
-
-// Broker type used for responses from the Service Manager client
-type Broker struct {
-	ID        string `json:"id"`
-	BrokerURL string `json:"broker_url"`
-
-	Catalog  *osbc.CatalogResponse      `json:"catalog"`
-	Metadata map[string]json.RawMessage `json:"metadata,omitempty"`
+func TestSm(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Sm Suite")
 }
