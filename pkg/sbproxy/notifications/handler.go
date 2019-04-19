8be24c1f0caabec687187c5753e63b23c014a56d
Peter Dotchev <dotchev@gmail.com>
2019-04-19 17:13:04 +0300

    WS tests running

--- pkg/sbproxy/notifications/handler.go
+++ pkg/sbproxy/notifications/handler.go
@@ -34,7 +34,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var lastNotificationGone = errors.New("Last notification revision no longer present in SM")
+var errLastNotificationGone = errors.New("Last notification revision no longer present in SM")
 
 type Handler struct {
 	ctx                      context.Context
@@ -52,46 +52,55 @@ func NewHandler(ctx context.Context, settings *sm.Settings) *Handler {
 	}
 }
 
-func (h *Handler) Start(fullResyncChan chan struct{}, notificationsQueue Queue) {
+func (h *Handler) Start(resyncChan chan struct{}, notificationsQueue Queue) {
+	go h.run(resyncChan, notificationsQueue)
+}
+
+func (h *Handler) run(resyncChan chan struct{}, notificationsQueue Queue) {
 	for {
 		var err error
 		h.conn, err = h.dial()
 		if err != nil {
-			if err == lastNotificationGone {
-				fullResyncChan <- struct{}{}
+			if err == errLastNotificationGone {
+				resyncChan <- struct{}{}
 				continue
 			}
 			select {
 			case <-h.ctx.Done():
-				log.C(h.ctx).Info("notifications: stopping connect attempts")
+				log.C(h.ctx).Info("Stopping connect attempts")
 				return
 			case <-time.After(time.Second * 5):
 				continue
 			}
 		}
-		h.conn.SetReadDeadline(time.Now().Add(h.pongTimeout))
+		done := make(chan struct{}, 1)
+		h.conn.SetReadDeadline(time.Now().Add(time.Second + h.pongTimeout))
 		h.conn.SetPongHandler(func(string) error {
+			log.C(h.ctx).Debug("Received pong")
 			h.conn.SetReadDeadline(time.Now().Add(h.pongTimeout))
 			return nil
 		})
 		childContext, stopChildren := context.WithCancel(h.ctx)
-		done := make(chan struct{}, 1)
-		go h.wsReader(childContext, notificationsQueue, done)
-		go h.wsWriter(childContext, done)
+		go h.NotificationReader(childContext, notificationsQueue, done)
+		go h.Pinger(childContext, done)
 
 		<-done // wait for at least one child goroutine (reader/writer) to exit
 		stopChildren()
-		h.conn.WriteMessage(websocket.CloseMessage, []byte{})
+		log.C(h.ctx).Debug("Closing websocket")
+		h.conn.WriteControl(websocket.CloseMessage, []byte{}, time.Now().Add(5*time.Second))
 		h.conn.Close()
 		if h.ctx.Err() != nil {
-			log.C(h.ctx).Info("Context cancelled. Terminating notifications handler")
+			log.C(h.ctx).WithError(h.ctx.Err()).Info("Context cancelled. Terminating notifications handler")
 			return
 		}
 	}
 }
 
-func (h *Handler) wsReader(ctx context.Context, notificationsQueue Queue, done chan<- struct{}) {
-	defer close(done)
+func (h *Handler) NotificationReader(ctx context.Context, notificationsQueue Queue, done chan<- struct{}) {
+	defer func() {
+		log.C(ctx).Debug("Exiting notification reader")
+		done <- struct{}{}
+	}()
 	for {
 		if ctx.Err() != nil {
 			return
@@ -107,13 +116,17 @@ func (h *Handler) wsReader(ctx context.Context, notificationsQueue Queue, done c
 			log.C(ctx).WithError(err).Error("Could not unmarshal WS message into a notification")
 			return
 		}
+		log.C(ctx).Debugf("Received notification with revision %d", notification.Revision)
 		notificationsQueue.Push(&notification)
 		h.lastNotificationRevision = notification.Revision
 	}
 }
 
-func (h *Handler) wsWriter(ctx context.Context, done chan<- struct{}) {
-	defer close(done)
+func (h *Handler) Pinger(ctx context.Context, done chan<- struct{}) {
+	defer func() {
+		log.C(ctx).Debug("Exiting pinger")
+		done <- struct{}{}
+	}()
 	ticker := time.NewTicker(h.pingPeriod)
 	defer ticker.Stop()
 	for {
@@ -121,7 +134,9 @@ func (h *Handler) wsWriter(ctx context.Context, done chan<- struct{}) {
 		case <-ctx.Done():
 			return
 		case <-ticker.C:
+			log.C(ctx).Debug("Sending ping")
 			if err := h.conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				log.C(ctx).WithError(err).Error("Could not write message on the websocket")
 				return
 			}
 		}
@@ -144,7 +159,9 @@ func (h *Handler) dial() (*websocket.Conn, error) {
 	}
 	url.Path = path.Join(url.Path, h.smSettings.NotificationsAPIPath)
 	if h.lastNotificationRevision > 0 {
-		url.Query().Set("last_notification_revision", strconv.FormatInt(h.lastNotificationRevision, 10))
+		q := url.Query()
+		q.Set("last_notification_revision", strconv.FormatInt(h.lastNotificationRevision, 10))
+		url.RawQuery = q.Encode()
 	}
 	dialer := &websocket.Dialer{
 		Proxy:            http.ProxyFromEnvironment,
@@ -153,19 +170,20 @@ func (h *Handler) dial() (*websocket.Conn, error) {
 			InsecureSkipVerify: h.smSettings.SkipSSLValidation,
 		},
 	}
+	log.C(h.ctx).Debugf("Connecting to %s ...", url)
 	conn, resp, err := dialer.DialContext(h.ctx, url.String(), headers)
 	if err != nil {
-		if resp != nil {
-			log.C(h.ctx).Errorf("notifications: could not connect to %s: %v status: %d", url, err, resp.StatusCode)
+		if resp == nil {
+			log.C(h.ctx).WithError(err).Errorf("Could not connect to %s", url)
+		} else {
+			log.C(h.ctx).WithError(err).Errorf("Could not connect to %s: status: %d", url, resp.StatusCode)
 			if resp.StatusCode == http.StatusGone {
 				h.lastNotificationRevision = 0
-				return nil, lastNotificationGone
+				return nil, errLastNotificationGone
 			}
 			if resp.StatusCode >= 400 && resp.StatusCode < 500 {
 				// TODO: break ws loop as this is unrecoverable and indicate on health endpoint
 			}
-		} else {
-			log.C(h.ctx).Errorf("notifications: could not connect to %s: %v", url, err)
 		}
 		return nil, err
 	}
@@ -184,6 +202,7 @@ func (h *Handler) dial() (*websocket.Conn, error) {
 		return nil, err
 	}
 	h.pingPeriod = (maxPingPeriod * 2) / 3
-	h.pongTimeout = (h.pingPeriod * 11) / 10 // should be longer than pingPeriod
+	h.pongTimeout = (h.pingPeriod * 13) / 10 // should be longer than pingPeriod
+	log.C(h.ctx).Debugf("Ping period: %s pong timeout: %s", h.pingPeriod, h.pongTimeout)
 	return conn, nil
 }
