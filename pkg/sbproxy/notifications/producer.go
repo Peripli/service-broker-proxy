b0591908cf313a60dd8bd42bdd9141a980167c7c
dpanayotov <dpanayotov94@gmail.com>
2019-04-23 14:49:45 +0300

    Fix connection leak

--- pkg/sbproxy/notifications/producer.go
+++ pkg/sbproxy/notifications/producer.go
@@ -51,10 +51,12 @@ type Producer struct {
 
 // ProducerSettings are the settings for the producer
 type ProducerSettings struct {
-	MinPingPeriod        time.Duration `mapstructure:"min_ping_period"`
-	ReconnectDelay       time.Duration `mapstructure:"reconnect_delay"`
-	PongTimeout          time.Duration `mapstructure:"pong_timeout"`
-	PingPeriodPercentage int64         `mapstructure:"ping_period_percentage"`
+	// MinPingPeriod is the minimum period
+	MinPingPeriod  time.Duration `mapstructure:"min_ping_period"`
+	ReconnectDelay time.Duration `mapstructure:"reconnect_delay"`
+	PongTimeout    time.Duration `mapstructure:"pong_timeout"`
+	// PingPeriodPercentage is the percentage of actual ping period compared to the max_ping_period returned by SM
+	PingPeriodPercentage int64 `mapstructure:"ping_period_percentage"`
 }
 
 // Validate validates the producer settings
@@ -122,56 +124,48 @@ func buildNotificationsURL(baseURL, notificationsPath string) (*url.URL, error) 
 }
 
 // Start starts the producer in a new go-routine
-func (h *Producer) Start(ctx context.Context, messages chan *Message) {
-	go h.run(ctx, messages)
+func (p *Producer) Start(ctx context.Context, messages chan *Message) {
+	go p.run(ctx, messages)
 }
 
-func (h *Producer) run(ctx context.Context, messages chan *Message) {
+func (p *Producer) run(ctx context.Context, messages chan *Message) {
 	for {
-		needResync := h.lastNotificationRevision == 0
-		var err error
-		h.conn, err = h.dial(ctx)
-		if err != nil {
+		needResync := p.lastNotificationRevision == 0
+		if err := p.connect(ctx); err != nil {
 			log.C(ctx).WithError(err).Error("could not connect websocket")
 			if err == errLastNotificationGone { // skip reconnect delay
-				h.lastNotificationRevision = 0
+				p.lastNotificationRevision = 0
 				continue
 			}
 		} else {
 			if needResync {
 				messages <- &Message{Resync: true}
 			}
-			h.conn.SetPongHandler(func(string) error {
+			p.conn.SetPongHandler(func(string) error {
 				log.C(ctx).Debug("Received pong")
-				return h.conn.SetReadDeadline(time.Now().Add(h.readTimeout))
+				return p.conn.SetReadDeadline(time.Now().Add(p.readTimeout))
 			})
 
 			done := make(chan struct{}, 1)
 			childContext, stopChildren := context.WithCancel(ctx)
-			go h.readNotifications(childContext, messages, done)
-			go h.ping(childContext, done)
+			go p.readNotifications(childContext, messages, done)
+			go p.ping(childContext, done)
 
 			<-done // wait for at least one child goroutine (reader/writer) to exit
 			stopChildren()
-			log.C(ctx).Debug("Closing websocket connection")
-			if err = h.conn.WriteControl(websocket.CloseMessage, []byte{}, time.Now().Add(h.smSettings.RequestTimeout)); err != nil {
-				log.C(ctx).WithError(err).Warn("Could not send close message on websocket")
-			}
-			if err = h.conn.Close(); err != nil {
-				log.C(ctx).WithError(err).Warn("Could not close websocket connection")
-			}
+			p.closeConnection(ctx)
 		}
 		select {
 		case <-ctx.Done():
 			log.C(ctx).Info("Context cancelled. Terminating notifications handler")
 			return
-		case <-time.After(h.producerSettings.ReconnectDelay):
+		case <-time.After(p.producerSettings.ReconnectDelay):
 			log.C(ctx).Debug("Attempting to reestablish websocket connection")
 		}
 	}
 }
 
-func (h *Producer) readNotifications(ctx context.Context, messages chan *Message, done chan<- struct{}) {
+func (p *Producer) readNotifications(ctx context.Context, messages chan *Message, done chan<- struct{}) {
 	defer func() {
 		log.C(ctx).Debug("Exiting notification reader")
 		done <- struct{}{}
@@ -180,11 +174,11 @@ func (h *Producer) readNotifications(ctx context.Context, messages chan *Message
 		if ctx.Err() != nil {
 			return
 		}
-		if err := h.conn.SetReadDeadline(time.Now().Add(h.readTimeout)); err != nil {
+		if err := p.conn.SetReadDeadline(time.Now().Add(p.readTimeout)); err != nil {
 			log.C(ctx).WithError(err).Error("Error setting read timeout on websocket")
 			return
 		}
-		_, bytes, err := h.conn.ReadMessage()
+		_, bytes, err := p.conn.ReadMessage()
 		if err != nil {
 			log.C(ctx).WithError(err).Error("Error reading from websocket")
 			return
@@ -196,16 +190,16 @@ func (h *Producer) readNotifications(ctx context.Context, messages chan *Message
 		}
 		log.C(ctx).Debugf("Received notification with revision %d", notification.Revision)
 		messages <- &Message{Notification: &notification, Resync: false}
-		h.lastNotificationRevision = notification.Revision
+		p.lastNotificationRevision = notification.Revision
 	}
 }
 
-func (h *Producer) ping(ctx context.Context, done chan<- struct{}) {
+func (p *Producer) ping(ctx context.Context, done chan<- struct{}) {
 	defer func() {
 		log.C(ctx).Debug("Exiting pinger")
 		done <- struct{}{}
 	}()
-	ticker := time.NewTicker(h.pingPeriod)
+	ticker := time.NewTicker(p.pingPeriod)
 	defer ticker.Stop()
 	for {
 		select {
@@ -213,7 +207,7 @@ func (h *Producer) ping(ctx context.Context, done chan<- struct{}) {
 			return
 		case <-ticker.C:
 			log.C(ctx).Debug("Sending ping")
-			if err := h.conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+			if err := p.conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
 				log.C(ctx).WithError(err).Error("Could not write message on the websocket")
 				return
 			}
@@ -221,59 +215,82 @@ func (h *Producer) ping(ctx context.Context, done chan<- struct{}) {
 	}
 }
 
-func (h *Producer) dial(ctx context.Context) (*websocket.Conn, error) {
+func (p *Producer) connect(ctx context.Context) error {
 	headers := http.Header{}
-	auth := "Basic " + base64.StdEncoding.EncodeToString([]byte(h.smSettings.User+":"+h.smSettings.Password))
+	auth := "Basic " + base64.StdEncoding.EncodeToString([]byte(p.smSettings.User+":"+p.smSettings.Password))
 	headers.Add("Authorization", auth)
 
-	connectURL := *h.url
-	if h.lastNotificationRevision > 0 {
+	connectURL := *p.url
+	if p.lastNotificationRevision > 0 {
 		q := connectURL.Query()
-		q.Set("last_notification_revision", strconv.FormatInt(h.lastNotificationRevision, 10))
+		q.Set("last_notification_revision", strconv.FormatInt(p.lastNotificationRevision, 10))
 		connectURL.RawQuery = q.Encode()
 	}
 	dialer := &websocket.Dialer{
 		Proxy:            http.ProxyFromEnvironment,
-		HandshakeTimeout: h.smSettings.RequestTimeout,
+		HandshakeTimeout: p.smSettings.RequestTimeout,
 		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: h.smSettings.SkipSSLValidation,
+			InsecureSkipVerify: p.smSettings.SkipSSLValidation,
 		},
 	}
 	log.C(ctx).Debugf("Connecting to %s ...", &connectURL)
-	conn, resp, err := dialer.DialContext(ctx, connectURL.String(), headers)
+	var err error
+	var resp *http.Response
+	p.conn, resp, err = dialer.DialContext(ctx, connectURL.String(), headers)
 	if err != nil {
 		if resp == nil {
 			log.C(ctx).WithError(err).Errorf("Could not connect to %s", &connectURL)
 		} else {
 			log.C(ctx).WithError(err).Errorf("Could not connect to %s: status: %d", &connectURL, resp.StatusCode)
 			if resp.StatusCode == http.StatusGone {
-				return nil, errLastNotificationGone
+				return errLastNotificationGone
 			}
 		}
-		return nil, err
+		return err
+	}
+	if err = p.readResponseHeaders(ctx, resp.Header); err != nil {
+		p.closeConnection(ctx)
+		return err
 	}
 
+	return nil
+}
+
+func (p *Producer) readResponseHeaders(ctx context.Context, header http.Header) error {
 	// TODO: define constants for these headers in service-manager
-	if h.lastNotificationRevision == 0 {
-		revision, err := strconv.ParseInt(resp.Header.Get("last_notification_revision"), 10, 64)
+	if p.lastNotificationRevision == 0 {
+		revision, err := strconv.ParseInt(header.Get("last_notification_revision"), 10, 64)
 		if err != nil {
-			return nil, err
+			return err
 		}
 		if revision <= 0 {
-			return nil, fmt.Errorf("invalid last notification revision received (%d)", revision)
+			return fmt.Errorf("invalid last notification revision received (%d)", revision)
 		}
-		h.lastNotificationRevision = revision
+		p.lastNotificationRevision = revision
 	}
 
-	maxPingPeriod, err := time.ParseDuration(resp.Header.Get("max_ping_period"))
+	maxPingPeriod, err := time.ParseDuration(header.Get("max_ping_period"))
 	if err != nil {
-		return nil, err
+		return err
 	}
-	if maxPingPeriod < h.producerSettings.MinPingPeriod {
-		return nil, fmt.Errorf("invalid max ping period (%s) must be greater than the minimum ping period (%s)", maxPingPeriod, h.producerSettings.MinPingPeriod)
+	if maxPingPeriod < p.producerSettings.MinPingPeriod {
+		return fmt.Errorf("invalid max ping period (%s) must be greater than the minimum ping period (%s)", maxPingPeriod, p.producerSettings.MinPingPeriod)
 	}
-	h.pingPeriod = time.Duration(int64(maxPingPeriod) * h.producerSettings.PingPeriodPercentage / 100)
-	h.readTimeout = h.pingPeriod + h.producerSettings.PongTimeout // should be longer than pingPeriod
-	log.C(ctx).Debugf("Ping period: %s pong timeout: %s", h.pingPeriod, h.readTimeout)
-	return conn, nil
+	p.pingPeriod = time.Duration(int64(maxPingPeriod) * p.producerSettings.PingPeriodPercentage / 100)
+	p.readTimeout = p.pingPeriod + p.producerSettings.PongTimeout // should be longer than pingPeriod
+	log.C(ctx).Debugf("Ping period: %s pong timeout: %s", p.pingPeriod, p.readTimeout)
+	return nil
+}
+
+func (p *Producer) closeConnection(ctx context.Context) {
+	if p.conn == nil {
+		return
+	}
+	log.C(ctx).Debug("Closing websocket connection")
+	if err := p.conn.WriteControl(websocket.CloseMessage, []byte{}, time.Now().Add(p.smSettings.RequestTimeout)); err != nil {
+		log.C(ctx).WithError(err).Warn("Could not send close message on websocket")
+	}
+	if err := p.conn.Close(); err != nil {
+		log.C(ctx).WithError(err).Warn("Could not close websocket connection")
+	}
 }
