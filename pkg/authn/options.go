ea475e13fbf8eee35f69c6d286170f142bbaad33
kkabakchiev <kiril.kabakchiev@sap.com>
2019-09-20 11:43:53 +0300

    fix linter issues

--- pkg/authn/options.go
+++ pkg/authn/options.go
@@ -1,11 +1,13 @@
 package authn
 
+// DefaultSettings for the authentication config
 func DefaultSettings() *Settings {
 	return &Settings{
 		SkipSSLValidation: false,
 	}
 }
 
+// Settings for authentication confnig
 type Settings struct {
 	User              string `mapstructure:"user"`
 	Password          string `mapstructure:"password"`
