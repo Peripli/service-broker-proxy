1444b75d722e2085d941c2fece62bb986fc14b75
Nikolay Stoykov <nikolay.stoykov@sap.com>
2020-01-16 13:48:38 +0200

    Adapt tests

--- pkg/authn/basic_test.go
+++ pkg/authn/basic_test.go
@@ -19,8 +19,8 @@ package authn
 import (
 	"net/http"
 
-	"github.com/Peripli/service-manager/pkg/util"
-	"github.com/Peripli/service-manager/pkg/web"
+	httpsec "github.com/Peripli/service-manager/pkg/security/http"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
@@ -34,7 +34,7 @@ var _ = Describe("Basic Authentication wrapper", func() {
 		invalidPassword = "invalidPassword"
 	)
 	var (
-		filter web.Filter
+		basic httpsec.Authenticator
 	)
 
 	newRequest := func(user, pass string) *http.Request {
@@ -45,35 +45,24 @@ var _ = Describe("Basic Authentication wrapper", func() {
 	}
 
 	BeforeEach(func() {
-		filter = NewBasicAuthnFilter(validUsername, validPassword)
+		basic = NewInMemoryAuthenticator(validUsername, validPassword)
 	})
 
 	DescribeTable("when given a request with basic authorization",
-		func(expectedStatus int, expectsError bool, username, password string) {
+		func(expectedDecision httpsec.Decision, expectsError bool, username, password string) {
 
 			request := newRequest(username, password)
-			response, err := filter.Run(&web.Request{Request: request}, testHandler())
-
+			_, decision, err := basic.Authenticate(request)
 			if expectsError {
 				Expect(err).To(HaveOccurred())
-				httpError := err.(*util.HTTPError)
-				Expect(httpError.StatusCode).To(Equal(expectedStatus))
+				Expect(decision).To(Equal(expectedDecision))
 			} else {
-				Expect(response.StatusCode).To(Equal(expectedStatus))
+				Expect(decision).To(Equal(expectedDecision))
 			}
 		},
-		Entry("returns 401 for empty username", http.StatusUnauthorized, true, "", validPassword),
-		Entry("returns 401 for empty password", http.StatusUnauthorized, true, validUsername, ""),
-		Entry("returns 401 for invalid credentials", http.StatusUnauthorized, true, invalidUser, invalidPassword),
-		Entry("returns 200 for valid credentials", http.StatusOK, false, validUsername, validPassword),
+		Entry("returns 401 for empty username", httpsec.Deny, true, "", validPassword),
+		Entry("returns 401 for empty password", httpsec.Deny, true, validUsername, ""),
+		Entry("returns 401 for invalid credentials", httpsec.Deny, true, invalidUser, invalidPassword),
+		Entry("returns 200 for valid credentials", httpsec.Allow, false, validUsername, validPassword),
 	)
 })
-
-func testHandler() web.HandlerFunc {
-	return web.HandlerFunc(func(request *web.Request) (*web.Response, error) {
-		return &web.Response{
-			StatusCode: 200,
-			Body:       []byte(`{}`),
-		}, nil
-	})
-}
